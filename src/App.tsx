1f8882374fbf5e76070f428d98f9c3e6a5345c01
maxfieldplays <aqhilnair@gmail.com>
2025-07-31 23:08:44 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -432,7 +432,7 @@ const PortfolioPage = ({ onVideoPlay, onBack }: {
         </div>
 
         {/* Video Grid */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3">
           {videoData.map((video, index) => (
             <VideoCard 
               key={video.id} 
